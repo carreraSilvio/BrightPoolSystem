f36daf832f4e620f8a0eee15e1d284fddecb3c04
carreraSilvio <carrera.silvio@gmail.com>
2024-05-26 11:38:51 +0100

    simplified drawing gizmos

--- Runtime/Spawning/Behaviours/SpawnPoint.cs
+++ Runtime/Spawning/Behaviours/SpawnPoint.cs
@@ -60,42 +60,11 @@ namespace BrightLib.Pooling.Runtime
 #if UNITY_EDITOR
         private void OnDrawGizmos()
         {
-            //Gizmos.color = Color.yellow;
-            //Gizmos.DrawWireSphere(transform.position, _safeSpawnDistance);
             UnityEditor.Handles.color = Color.white;
-            UnityEditor.Handles.Label(transform.position, $"{_indentifier}{transform.GetSiblingIndex()}");
+            //UnityEditor.Handles.Label(transform.position, $"{_indentifier}{transform.GetSiblingIndex()}");
+            UnityEditor.Handles.Label(transform.position, $"{name}");
 
             Gizmos.color = Color.white;
-            ////Draw "S"
-            //float width = 0.5f;
-            //float height = 1f;
-            //// Define the points for drawing the letter "S"
-            //Vector3[] points = new Vector3[6];
-
-            //// Start point
-            //points[0] = new Vector3(width, 0f, 0);
-
-            //// Top curve of "S"
-            //points[1] = new Vector3(0f, 0f, 0);
-
-            //// Midpoint
-            //points[2] = new Vector3(0f, -height * 0.5f, 0);
-
-            //// Bottom curve of "S"
-            //points[3] = new Vector3(width, -height * 0.5f, 0);
-
-            //// End point
-            //points[4] = new Vector3(width, -height, 0);
-
-            //// End point
-            //points[5] = new Vector3(0f, -height, 0);
-
-            //// Draw the letter "S" using Gizmos.DrawLine
-            //Vector3 offset = new Vector2(1.1f, 1f);
-            //for (int i = 0; i < points.Length - 1; i++)
-            //{
-            //    Gizmos.DrawLine(transform.position + points[i] + offset, transform.position + points[i + 1] + offset);
-            //}
 
             //Draw sphere around
             Gizmos.DrawWireSphere(transform.position, 1f);
@@ -106,8 +75,8 @@ namespace BrightLib.Pooling.Runtime
             Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(transform.position, _safeSpawnDistance);
             UnityEditor.Handles.color = Color.white;
-            UnityEditor.Handles.Label(transform.position, $"{_indentifier}{transform.GetSiblingIndex()}");
-            //UnityEditor.Handles.Label(transform.position, $"{name}");
+            //UnityEditor.Handles.Label(transform.position, $"{_indentifier}{transform.GetSiblingIndex()}");
+            UnityEditor.Handles.Label(transform.position, $"{name}");
         }
 #endif
 
